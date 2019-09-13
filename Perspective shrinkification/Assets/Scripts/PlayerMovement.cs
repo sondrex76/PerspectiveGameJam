dab92cbe3febe62a61390260d32bd7bf6332641c
sondrex <sondrewr@stud.ntnu.no>
2019-09-13 18:43:41 +0200

    Added movement scrscript(near empty)

---
+++ Perspective shrinkification/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Serialized values
+    
+    // Movement speed
+    [Range(1, 20)]
+    [SerializeField]
+    float movementSpeed = 10.0f;    
+    
+    // Runnning speed
+    [Range(2, 30)]
+    [SerializeField]
+    float runningSpeed = 12.0f;
+    [SerializeField]
+    float minSize = 1.0f;
+    [SerializeField]
+    float maxSize = 21.0f;
+
+    // Private values
+    Rigidbody2D playerCollision;    // Player collission
+    Camera cameraElement;           // Camera(for perspective changes)
+    bool isRunning = false;
+    float currentSize = 1.0f;       // Current player size
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // Changes size to the specfied value
+    void changeSize(float newSize)
+    {
+
+    }
+}
