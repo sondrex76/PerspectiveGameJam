1c5f5b8a06c33ce0a4b03a158388bb23b5eca625
sondrex <sondrewr@stud.ntnu.no>
2019-09-14 17:37:07 +0200

    Minor correction to spider movement, still won't trigger in the larger collider for some khjfhkjdsfhjghjfgjhfghjbjygk reasongikøfdiuøfljhgufgku I may or may not be very very very pissed off.

--- Perspective shrinkification/Assets/Scripts/Spider.cs
+++ Perspective shrinkification/Assets/Scripts/Spider.cs
@@ -7,6 +7,8 @@ public class Spider : MonoBehaviour
     [SerializeField]
     Transform player;                   // The player's field, to know the player's current size
     [SerializeField]
+    Rigidbody2D spiderBody;             // The rigidbody of spider
+    [SerializeField]
     float sizeThresholdRun;             // Threshold where the spider starts running away(above this value)
     [SerializeField]
     float sizeThresholdFollow;          // Threshold where the spider starts running towards you(below this vlaue)
@@ -20,15 +22,33 @@ public class Spider : MonoBehaviour
     {
         
     }
-
-
-    // When something enters their view
-    private void OnCollisionStay(Collision collision)
+    // Why does this not fricking trigger when the player enters? I am so confused
+    private void OnCollisionStay2D(Collision2D collision)
     {
-        // CHecks if it were the player
         if (collision.gameObject.tag == "Player")
         {
+            Vector2 speedBase = (((Vector2)player.transform.position - (Vector2)transform.position).normalized);
 
+            speedBase.y = 0;
+            speedBase = speedBase.normalized;
+
+            if (player.transform.localScale.x < sizeThresholdFollow)    // If it should attack
+            {
+                spiderBody.velocity = speedBase * spiderVelocity;
+            }
+            else if (player.transform.localScale.x > sizeThresholdRun)  // If it should run
+            {
+                spiderBody.velocity = -speedBase * spiderVelocity;
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            spiderBody.velocity = new Vector2();
         }
     }
 }
+
